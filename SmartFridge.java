b7aabf31c10847d258bb4ea5a441c1e1a2e4322b
Sybitten <martyyung@hotmail.com>
2016-03-30 13:35:25 -0700

    30mar2016-2

--- SmartFridge.java
+++ SmartFridge.java
@@ -1,15 +1,50 @@
 import java.util.Scanner;
 
+//default units:
+//litres
+//kilograms
+//pounds
+//units (4 tomatoes)
+
 public class SmartFridge{
 	public static void main(String[] args){
-		System.out.println("Welcome to the SmartFridge.");
-		
-		printMenu();
-		
-		
+      //sets a default value
+      int userOption = -1;
+      
+      while(userOption != 5){
+         System.out.println("Welcome to the SmartFridge.");
+         
+         //prints the menu of options
+         System.out.println("\nWhat do you want to do today?");
+         
+         //prints main menu
+         printMenu();
+         
+         //gets valid input
+         //sends in 5 for 5 options
+         userOption = getInput(5);
+         
+         //determines where to go depending on what the user entered
+         switch(userOption){
+            case 1:
+               addItem();
+               break;
+            case 2:
+               removeItem();
+               break;
+            case 3:
+               break;
+            case 4:
+               break;
+         }//end switchcase
+         
+      }//end while check
+      
+      //these only get run when the user enters 5
+      System.out.println("Bye!");
+      System.exit(0);
+      
 	}//end main
-	
-	
    
    //requires
    //1. add item
@@ -20,20 +55,17 @@ public class SmartFridge{
    //5. exit program
    
 	
-	public static void printMenu(){
-		String optionPreCheck = "";
-		int userOption= -1;
-		Scanner sc = new Scanner(System.in);
-		
-      //prints the menu of options
-		System.out.println("\nWhat do you want to do today?");
-		
-      //gets valid input
-      //sends in 5 for 5 options
-      getInput(5);
-		
+	public static void printMenu(){		
+      //clean this later
+      System.out.println("\n1. Add item to fridge");
+      System.out.println("2. Remove item from fridge");
+      System.out.println("3. Display all contents of the fridge");
+      System.out.println("4. Enter a recipe");
+      System.out.println("5. Exit the fridge");
 	}//end printMenu
 	
+   
+   //options is the number of options the menu should have
 	public static int getInput(int options){
       Scanner sc = new Scanner(System.in);
       int input = -1;
@@ -45,22 +77,37 @@ public class SmartFridge{
          //checks if the scanner holds an int
          while(sc.hasNextInt() == false){
             sc.next();
-            System.out.println("Please enter a valid menu (1-" + option + ")");
+            System.out.println("Please enter a valid menu (1-" + options + ")");
          }
          
          input = sc.nextInt();
          
          //checks if the value inputted meets range
-         if(input >= 0 && input <=6){
+         if(input > 0 && input <= options){
             validInput = true;
             return input;
          }
          
-         System.out.println("Please enter a valid column number (0-6).");
+         System.out.println("Please enter a valid menu (1-" + options + ")");
       }//end validInput while
       
       return -1;
+   }//end getInput
+	
+   
+   public static void addItem(){
+      String newItem;
+      double quantity;
+      
+      System.out.println("What would you like to put in?");
+      
+      return;
    }
-	
-	
+   
+   
+   public static void removeItem(){
+      System.out.println("What would you like to remove?");
+      return;
+   }
+   
 }//end class
