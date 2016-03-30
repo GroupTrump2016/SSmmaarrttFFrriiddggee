adbf878f0cbac0218b3320368cf5c6b58357091b
Sybitten <martyyung@hotmail.com>
2016-03-30 16:15:04 -0700

    30mar2016-5

--- SmartFridge.java
+++ SmartFridge.java
@@ -22,9 +22,6 @@ public class SmartFridge{
       String[] items = new String[5];
       double[] quantity = new double[5];
       
-      items[1] = "Pizza";
-      quantity[1] = 2;
-      
       //check if null values or "0" quantities are in the array
       //should also sort the fridge
       
@@ -102,6 +99,8 @@ public class SmartFridge{
          System.out.println("Please enter a valid menu (1-" + options + ")");
       }//end validInput while
       
+      //will only get here by some magic error
+      sc.close();
       return -1;
    }//end getInput
 	
@@ -128,11 +127,19 @@ public class SmartFridge{
    
    public static void getValidUnits(){
       //checks if the user entered mL, L, g, mg, lb, no units, ignoring uppercase
+      boolean valid = false;
+      String userUnit;
+      Scanner sc = new Scanner(System.in);
       
-   }
+      while(!valid){
+         System.out.println("Valid units = mL, L, g, mg, lb, quantity:");
+         userUnit = sc.nextLine();
+         
+      }
+   }//end getValidUnits
    
    
-   public static double convertUnits(double quantity){
+   public static double convertUnits(double quantity, String userUnit){
       double converted = -1;
       
       //converts units here
@@ -148,13 +155,12 @@ public class SmartFridge{
    
    public static void printFridge(String[] items, double[] quantity){
       for(int i = 0; i < items.length; i++){
-         //this if check doesn't work yet
-         //suppose to print the item if the item isn't null
-         if(!"null".equals(items[i])){
-            System.out.println(i + items[i] + " | " + quantity[i]);
+         //checks if the item is null or actually has something
+         if(items[i] != null){
+            //make this prettier later
+            System.out.println(items[i] + " | " + quantity[i]);
          }
       }//end for loop
-      
    }//end printFridge
    
 }//end class
