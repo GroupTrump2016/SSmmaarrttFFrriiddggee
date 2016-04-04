82d130e4ac1fe8095a6dec0b0753efa58499d4f7
Sybitten <martyyung@hotmail.com>
2016-04-03 20:37:10 -0700

    moved many methods into a separate class to reduce clutter

---
+++ InputAndTools.java
@@ -0,0 +1,258 @@
+import java.util.Scanner;
+
+//contains a collection of tools needed for SmartFridge
+//it's all here to reduce clutter on the main class
+public class InputAndTools{
+   
+   //options is the number of options the menu should have
+   //can be reused for submenus (if needed)
+	public static int getMenuInput(int options){
+      Scanner sc = new Scanner(System.in);
+      int input = -1;
+      boolean validInput = false;
+      
+      System.out.println("Select a menu item from 1 to " + options + ":");
+      
+      while(!validInput){
+         //checks if the scanner holds an int
+         while(!sc.hasNextInt()){
+            sc.next();
+            System.out.println("Please enter a valid menu item (1-" + options + ")");
+         }
+         
+         input = sc.nextInt();
+         
+         //checks if the value inputted meets range
+         if(input > 0 && input <= options){
+            validInput = true;
+            return input;
+         }
+         
+         System.out.println("Please enter a valid menu item (1-" + options + ")");
+      }//end validInput while
+      
+      //will only get here by some magic error
+      return -1;
+   }//end getInput
+   
+   //gets a yes or no from the user
+   public static boolean yesOrNo(){
+      Scanner sc = new Scanner(System.in);
+      boolean validInput = false;
+      char input = 'A';
+      String check;
+      
+      //gets a valid char Y or N from the user
+      while(!validInput){
+         System.out.println("Please enter Y (Yes) or N (No):");
+         check = sc.next();
+         
+         while(check.length() > 1){
+            System.out.println("Please enter Y (Yes) or N (No):");
+            check = sc.next();
+         }
+         
+         input = check.charAt(0);
+         
+         //checks if the value inputted is Y or N
+         if(input == 'Y' || input == 'y'){
+            validInput = true;
+            return true;
+         }
+         
+         else if(input == 'N' || input == 'n'){
+            validInput = true;
+            return false;
+         }
+      }
+      
+      return false;
+   }
+   
+   
+   //gets a double from the user + checks that it is a double and not a string or something
+   public static double getValidQuantity(){
+      Scanner sc = new Scanner(System.in);
+      double quantity = -1;
+      
+      while(!sc.hasNextDouble()){
+         sc.next();
+         System.out.println("Please enter a valid quantity:");
+      }
+      
+      quantity = sc.nextDouble();
+      
+      return quantity;
+   }//end getValidQuantity
+   
+   
+   public static String getValidUnit(){
+      //checks if the user entered mL, L, g, mg, lb, no units, ignoring uppercase
+      boolean valid = false;
+      String userUnit = "";
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.println("Valid units = mL, L, g, mg, lb, or leave blank for quantity (eg. 3 Potatoes):");
+      userUnit = sc.nextLine();
+      
+      //need a check to see if the user is entering valid units
+      while(!valid){
+         if(userUnit.equalsIgnoreCase("mL")){
+            valid = true;
+         }
+         
+         else if(userUnit.equalsIgnoreCase("L")){
+            valid = true;
+         }
+         
+         else if(userUnit.equalsIgnoreCase("g")){
+            valid = true;
+         }
+         
+         else if(userUnit.equalsIgnoreCase("mg")){
+            valid = true;
+         }
+         
+         else if(userUnit.equalsIgnoreCase("lb")){
+            valid = true;
+         }
+         
+         else if(userUnit.equalsIgnoreCase("")){
+            valid = true;
+         }
+         
+         else{
+            System.out.println("Please enter valid units.");
+            System.out.println("Valid units = mL, L, g, mg, lb, or leave blank for quantity (eg. 3 Potatoes):");
+            userUnit = sc.nextLine();
+         }
+      }
+      
+      return userUnit;
+      
+   }//end getValidUnit
+   
+   
+   //checks if the user is entering mg <-> g, mL <-> L, lb <-> lb, quantity <-> quantity
+   //NOT mg <-> L, etc.
+   public static boolean doUnitsMatch(String userUnit, String[] units, int itemPos){
+      String fridgeUnit = units[itemPos];
+      
+      //checks if the userUnit is the same as fridgeUnit
+      if(userUnit.equalsIgnoreCase(fridgeUnit)){
+         return true;
+      }
+      
+      //checks mL <-> L
+      if(userUnit.equalsIgnoreCase("mL") && fridgeUnit.equalsIgnoreCase("L")){
+         return true;
+      }
+      else if(userUnit.equalsIgnoreCase("L") && fridgeUnit.equalsIgnoreCase("mL")){
+         return true;
+      }
+      
+      //checks mg <-> g
+      if(userUnit.equalsIgnoreCase("mg") && fridgeUnit.equalsIgnoreCase("g")){
+         return true;
+      }
+      else if(userUnit.equalsIgnoreCase("g") && fridgeUnit.equalsIgnoreCase("mg")){
+         return true;
+      }
+      
+      //checks lb
+      if(userUnit.equalsIgnoreCase("lb") && fridgeUnit.equalsIgnoreCase("lb")){
+         return true;
+      }
+      
+      //checks quantity
+      if(userUnit.equalsIgnoreCase("") && fridgeUnit.equalsIgnoreCase("")){
+         return true;
+      }
+      
+      return false;
+   }
+   
+   
+   //checks if anything in quantity is greater than 0
+   //returns true if it is empty, returns false if it isn't
+   public static boolean checkEmpty(double[] quantity){
+      for(int i = 0; i < quantity.length; i++){
+         if(quantity[i] > 0){
+            return false;
+         }
+      }
+      
+      return true;
+   }
+   
+   
+   //checks if the item is already in the fridge
+   //if it does, it returns the position
+   //if it doesn't, it returns -1
+   public static int getItemPos(String[] items, String newItem){
+      for(int i = 0; i < items.length; i++){
+         if(newItem.equalsIgnoreCase(items[i])){
+            return i;
+         }
+      }
+      
+      return -1;
+   }//end isItemPresent
+   
+   
+   //converts units to the unit in the fridge
+   public static double convertUnits(double quantity, String userUnit, int itemPos, String[] units){
+      double converted = -1;
+      String fridgeUnit;
+      
+      //find the unit being used at emptyPos
+      fridgeUnit = units[itemPos];
+      
+      //checks if the userUnit is the same as fridgeUnit
+      if(userUnit.equalsIgnoreCase(fridgeUnit)){
+         converted = quantity;
+      }
+      
+      //converts mL <-> L
+      if(userUnit.equalsIgnoreCase("mL") && fridgeUnit.equalsIgnoreCase("L")){
+         converted = quantity / 1000;
+      }
+      else if(userUnit.equalsIgnoreCase("L") && fridgeUnit.equalsIgnoreCase("mL")){
+         converted = quantity * 1000;
+      }
+      
+      //converts mg <-> g 
+      if(userUnit.equalsIgnoreCase("mg") && fridgeUnit.equalsIgnoreCase("g")){
+         converted = quantity / 1000;
+      }
+      else if(userUnit.equalsIgnoreCase("g") && fridgeUnit.equalsIgnoreCase("mg")){
+         converted = quantity * 1000;
+      }
+      
+      //returns the lb
+      if(userUnit.equalsIgnoreCase("lb") && fridgeUnit.equalsIgnoreCase("lb")){
+         converted = quantity;
+      }
+      
+      //returns the quantity
+      if(userUnit.equalsIgnoreCase("") && fridgeUnit.equalsIgnoreCase("")){
+         converted = quantity;
+      }
+      
+      return converted;
+   }//end convertUnits
+   
+   
+   //finds the first position where the quanitity is 0 so that a new item can be put in there
+   public static int getNullPosition(double[] quantity){
+      //checks entire fridge for elements with no quantity
+      for(int i = 0; i < quantity.length; i++){
+         //checks if the item has no quantity
+         if(quantity[i] == 0){
+            return i;
+         }
+      }
+      
+      return -1;
+   }//end getNullPosition
+}
--- SmartFridge.java
+++ SmartFridge.java
@@ -78,7 +78,7 @@ public class SmartFridge{
          
          //gets valid input
          //sends in 7 for 7 options
-         userOption = getMenuInput(7);
+         userOption = InputAndTools.getMenuInput(7);
          
          //determines where to go depending on what the user entered
          switch(userOption){
@@ -89,7 +89,7 @@ public class SmartFridge{
                }
                else{
                   //expands the arrays to make room for another item
-                  if(getNullPosition(quantity) == -1){
+                  if(InputAndTools.getNullPosition(quantity) == -1){
                      items = ExpandArray.expand(items);
                      quantity = ExpandArray.expand(quantity);
                      units = ExpandArray.expand(units);
@@ -101,7 +101,7 @@ public class SmartFridge{
                   break;
                }
             case 2://removes item
-               if(checkEmpty(quantity)){
+               if(InputAndTools.checkEmpty(quantity)){
                   System.out.println("\nYour fridge is empty!\nYou need stuff in there before you can take them out.");
                   break;
                }
@@ -116,7 +116,7 @@ public class SmartFridge{
                   break;
                }
             case 3://prints fridge
-               if(checkEmpty(quantity)){
+               if(InputAndTools.checkEmpty(quantity)){
                   System.out.println("\nYour fridge is empty!");
                   break;
                }
@@ -131,7 +131,7 @@ public class SmartFridge{
                System.out.println("Are you sure you want to remove ALL the items in the fridge?");
                
                //calls method to remove items (or not if they pick no)
-               if(yesOrNo()){
+               if(InputAndTools.yesOrNo()){
                   items = ShrinkArray.clear(items, DEFAULT_SIZE);
                   units = ShrinkArray.clear(units, DEFAULT_SIZE);
                   quantity = ShrinkArray.clear(quantity, DEFAULT_SIZE);
@@ -141,7 +141,6 @@ public class SmartFridge{
                else{
                   System.out.println("\nDid not remove.");
                }
-               
                
                break;
             case 6://imports file
@@ -189,7 +188,7 @@ public class SmartFridge{
       //these only get run when the user enters the PROGRAM_END value
       //must ask if user wants to save fridge contents into a file
       System.out.println("Would you like to save the current contents of the fridge?");
-      if(yesOrNo()){
+      if(InputAndTools.yesOrNo()){
          exportFridge(items, quantity, units);
       }
       
@@ -210,7 +209,9 @@ public class SmartFridge{
       System.out.println("7. Exit the fridge");
 	}//end printMenu
 	
-   
+   public static void printRecipeMenu(){
+      
+   }
    
    public static void exportFridge(String[] items, double[] quantity, String[] units) throws IOException{
       Scanner sc = new Scanner(System.in);
@@ -236,100 +237,6 @@ public class SmartFridge{
       pw.close();
    }
    
-   public static boolean yesOrNo(){
-      Scanner sc = new Scanner(System.in);
-      boolean validInput = false;
-      char input = 'A';
-      String check;
-      
-      //gets a valid Y or N from the user
-      while(!validInput){
-         System.out.println("Please enter Y (Yes) or N (No):");
-         check = sc.next();
-         
-         while(check.length() > 1){
-            System.out.println("Please enter Y (Yes) or N (No):");
-            check = sc.next();
-         }
-         
-         input = check.charAt(0);
-         
-         //checks if the value inputted is Y or N
-         if(input == 'Y' || input == 'y'){
-            validInput = true;
-            return true;
-         }
-         
-         else if(input == 'N' || input == 'n'){
-            validInput = true;
-            return false;
-         }
-      }
-      
-      return false;
-   }
-   
-   //options is the number of options the menu should have
-   //can be reused for submenus (if needed)
-	public static int getMenuInput(int options){
-      Scanner sc = new Scanner(System.in);
-      int input = -1;
-      boolean validInput = false;
-      
-      System.out.println("Select a menu item from 1 to " + options + ":");
-      
-      while(!validInput){
-         //checks if the scanner holds an int
-         while(!sc.hasNextInt()){
-            sc.next();
-            System.out.println("Please enter a valid menu item (1-" + options + ")");
-         }
-         
-         input = sc.nextInt();
-         
-         //checks if the value inputted meets range
-         if(input > 0 && input <= options){
-            validInput = true;
-            return input;
-         }
-         
-         System.out.println("Please enter a valid menu item (1-" + options + ")");
-      }//end validInput while
-      
-      //will only get here by some magic error
-      return -1;
-   }//end getInput
-	
-   
-   //gets a double from the user + checks that it is a double and not a string or something
-   public static double getValidQuantity(){
-      Scanner sc = new Scanner(System.in);
-      double quantity = -1;
-      
-      while(!sc.hasNextDouble()){
-         sc.next();
-         System.out.println("Please enter a valid quantity:");
-      }
-      
-      quantity = sc.nextDouble();
-      
-      return quantity;
-   }//end getValidQuantity
-   
-   
-   //finds the first position where the quanitity is 0 so that a new item can be put in there
-   public static int getNullPosition(double[] quantity){
-      //checks entire fridge for elements with no quantity
-      for(int i = 0; i < quantity.length; i++){
-         //checks if the item has no quantity
-         if(quantity[i] == 0){
-            return i;
-         }
-      }
-      
-      return -1;
-   }//end getNullPosition
-   
    
    public static void addItem(String[] items, double[] quantity, String[] units){
       //these variables are for the new item being put in
@@ -340,28 +247,28 @@ public class SmartFridge{
       String newItem = sc.nextLine();
                   
       System.out.println("What unit are you storing this in?");
-      String userUnit = getValidUnit();
+      String userUnit = InputAndTools.getValidUnit();
                   
       //check if the current item is in the fridge already
       //if not add it to an empty position
-      int itemPos = getItemPos(items, newItem);
+      int itemPos = InputAndTools.getItemPos(items, newItem);
                   
       //checks the unit matches the category of units in the fridge
       if(itemPos != -1){
-         while(!doUnitsMatch(userUnit, units, itemPos)){
+         while(!InputAndTools.doUnitsMatch(userUnit, units, itemPos)){
             System.out.println("There was a problem with the unit you selected. The item in the fridge is using " + units[itemPos] + ".");
             System.out.println("\nPlease make sure that you're storing the item in the same category of units as the fridge (eg. mg <-> g or mL <-> L)");
-            userUnit = getValidUnit();
+            userUnit = InputAndTools.getValidUnit();
          }
       }
       
       System.out.println("And how much of it will we be storing?");
-      double newQuantity = getValidQuantity();
+      double newQuantity = InputAndTools.getValidQuantity();
       
       if(itemPos == -1){
-         items[getNullPosition(quantity)] = newItem;
-         units[getNullPosition(quantity)] = userUnit;
-         quantity[getNullPosition(quantity)] = newQuantity;
+         items[InputAndTools.getNullPosition(quantity)] = newItem;
+         units[InputAndTools.getNullPosition(quantity)] = userUnit;
+         quantity[InputAndTools.getNullPosition(quantity)] = newQuantity;
          
          if(userUnit.equalsIgnoreCase("")){
             System.out.println("\nYou just stored " + newQuantity + " " + newItem);
@@ -373,7 +280,7 @@ public class SmartFridge{
       
       else{
          //convert units here
-         convertedQuantity = convertUnits(newQuantity, userUnit, itemPos, units);
+         convertedQuantity = InputAndTools.convertUnits(newQuantity, userUnit, itemPos, units);
          
          //new, converted quantity is set to the empty position
          quantity[itemPos] += convertedQuantity;
@@ -389,135 +296,6 @@ public class SmartFridge{
    }//end addItem
    
    
-   public static String getValidUnit(){
-      //checks if the user entered mL, L, g, mg, lb, no units, ignoring uppercase
-      boolean valid = false;
-      String userUnit = "";
-      Scanner sc = new Scanner(System.in);
-      
-      System.out.println("Valid units = mL, L, g, mg, lb, or leave blank for quantity (eg. 3 Potatoes):");
-      userUnit = sc.nextLine();
-      
-      //need a check to see if the user is entering valid units
-      while(!valid){
-         if(userUnit.equalsIgnoreCase("mL")){
-            valid = true;
-         }
-         
-         else if(userUnit.equalsIgnoreCase("L")){
-            valid = true;
-         }
-         
-         else if(userUnit.equalsIgnoreCase("g")){
-            valid = true;
-         }
-         
-         else if(userUnit.equalsIgnoreCase("mg")){
-            valid = true;
-         }
-         
-         else if(userUnit.equalsIgnoreCase("lb")){
-            valid = true;
-         }
-         
-         else if(userUnit.equalsIgnoreCase("")){
-            valid = true;
-         }
-         
-         else{
-            System.out.println("Please enter valid units.");
-            System.out.println("Valid units = mL, L, g, mg, lb, or leave blank for quantity (eg. 3 Potatoes):");
-            userUnit = sc.nextLine();
-         }
-      }
-      
-      return userUnit;
-      
-   }//end getValidUnits
-   
-   
-   //checks if the user is entering mg <-> g, mL <-> L, lb <-> lb, quantity <-> quantity
-   //NOT mg <-> L, etc.
-   public static boolean doUnitsMatch(String userUnit, String[] units, int itemPos){
-      String fridgeUnit = units[itemPos];
-      
-      //checks if the userUnit is the same as fridgeUnit
-      if(userUnit.equalsIgnoreCase(fridgeUnit)){
-         return true;
-      }
-      
-      //checks mL <-> L
-      if(userUnit.equalsIgnoreCase("mL") && fridgeUnit.equalsIgnoreCase("L")){
-         return true;
-      }
-      else if(userUnit.equalsIgnoreCase("L") && fridgeUnit.equalsIgnoreCase("mL")){
-         return true;
-      }
-      
-      //checks mg <-> g
-      if(userUnit.equalsIgnoreCase("mg") && fridgeUnit.equalsIgnoreCase("g")){
-         return true;
-      }
-      else if(userUnit.equalsIgnoreCase("g") && fridgeUnit.equalsIgnoreCase("mg")){
-         return true;
-      }
-      
-      //checks lb
-      if(userUnit.equalsIgnoreCase("lb") && fridgeUnit.equalsIgnoreCase("lb")){
-         return true;
-      }
-      
-      //checks quantity
-      if(userUnit.equalsIgnoreCase("") && fridgeUnit.equalsIgnoreCase("")){
-         return true;
-      }
-      
-      return false;
-   }
-   
-      
-   public static double convertUnits(double quantity, String userUnit, int itemPos, String[] units){
-      double converted = -1;
-      String fridgeUnit;
-      
-      //find the unit being used at emptyPos
-      fridgeUnit = units[itemPos];
-      
-      //checks if the userUnit is the same as fridgeUnit
-      if(userUnit.equalsIgnoreCase(fridgeUnit)){
-         converted = quantity;
-      }
-      
-      //converts mL <-> L
-      if(userUnit.equalsIgnoreCase("mL") && fridgeUnit.equalsIgnoreCase("L")){
-         converted = quantity / 1000;
-      }
-      else if(userUnit.equalsIgnoreCase("L") && fridgeUnit.equalsIgnoreCase("mL")){
-         converted = quantity * 1000;
-      }
-      
-      //converts mg <-> g 
-      if(userUnit.equalsIgnoreCase("mg") && fridgeUnit.equalsIgnoreCase("g")){
-         converted = quantity / 1000;
-      }
-      else if(userUnit.equalsIgnoreCase("g") && fridgeUnit.equalsIgnoreCase("mg")){
-         converted = quantity * 1000;
-      }
-      
-      //returns the lb
-      if(userUnit.equalsIgnoreCase("lb") && fridgeUnit.equalsIgnoreCase("lb")){
-         converted = quantity;
-      }
-      
-      //returns the quantity
-      if(userUnit.equalsIgnoreCase("") && fridgeUnit.equalsIgnoreCase("")){
-         converted = quantity;
-      }
-      
-      return converted;
-   }//end convertUnits
-   
-   
    public static void removeItem(String[] items, double[] quantity, String[] units){
       //these variables is for the new item being put in
       String removeItem, userUnit;
@@ -528,34 +306,34 @@ public class SmartFridge{
       System.out.println("What would you like to remove?");
       removeItem = sc.nextLine();
       
-      while(getItemPos(items, removeItem) == -1){
+      while(InputAndTools.getItemPos(items, removeItem) == -1){
          System.out.println("That item cannot be found in the fridge.\nPlease enter a new item:");
          removeItem = sc.nextLine();
       }
       
       //finds location of the item
-      itemPos = getItemPos(items, removeItem);
+      itemPos = InputAndTools.getItemPos(items, removeItem);
       
       System.out.println("What unit are you removing the item from?");
-      userUnit = getValidUnit();
+      userUnit = InputAndTools.getValidUnit();
       
       //checks to see if the user is taking the item out in a valid unit
-      while(!doUnitsMatch(userUnit, units, itemPos)){
+      while(!InputAndTools.doUnitsMatch(userUnit, units, itemPos)){
          System.out.println("There was a problem with the unit you selected. The item in the fridge is using " + units[itemPos] + ".");
          System.out.println("\nPlease make sure that you're removing the item in the same category of units as the fridge (eg. mg <-> g or mL <-> L)");
-         userUnit = getValidUnit();
+         userUnit = InputAndTools.getValidUnit();
       }
       
       System.out.println("And how much of it will we be removing?");
-      removeQuantity = getValidQuantity();
+      removeQuantity = InputAndTools.getValidQuantity();
       
-      while(quantity[itemPos] < convertUnits(removeQuantity, userUnit, itemPos, units)){
+      while(quantity[itemPos] < InputAndTools.convertUnits(removeQuantity, userUnit, itemPos, units)){
          System.out.println("You're attempting to remove more than what you have.\nPlease enter a new amount to remove:");
-         removeQuantity = getValidQuantity();
+         removeQuantity = InputAndTools.getValidQuantity();
       }
       
       //convert units here
-      convertedQuantity = convertUnits(removeQuantity, userUnit, itemPos, units);
+      convertedQuantity = InputAndTools.convertUnits(removeQuantity, userUnit, itemPos, units);
          
       //new, converted quantity is set to the empty position
       quantity[itemPos] -= convertedQuantity;
@@ -568,35 +346,6 @@ public class SmartFridge{
       }
       
    }//end removeItem
-   
-   
-   
-   
-   
-   //checks if the item is already in the fridge
-   //if it does, it returns the position
-   //if it doesn't, it returns -1
-   public static int getItemPos(String[] items, String newItem){
-      for(int i = 0; i < items.length; i++){
-         if(newItem.equalsIgnoreCase(items[i])){
-            return i;
-         }
-      }
-      
-      return -1;
-   }//end isItemPresent
-   
-   //checks if anything in quantity is greater than 0
-   //returns true if it is empty, returns false if it isn't
-   public static boolean checkEmpty(double[] quantity){
-      for(int i = 0; i < quantity.length; i++){
-         if(quantity[i] > 0){
-            return false;
-         }
-      }
-      
-      return true;
-   }
    
    
    public static void importFile(String[] items, double[] quantity, String[] units, int MAX_SIZE, Scanner fileSc) throws IOException{
@@ -631,11 +380,11 @@ public class SmartFridge{
       
       //check if the current item is in the fridge already
       //if not add it to an empty position
-      int itemPos = getItemPos(items, newItem);
+      int itemPos = InputAndTools.getItemPos(items, newItem);
       
       //checks the unit matches the category of units in the fridge
       if(itemPos != -1){
-         if(!doUnitsMatch(newUnit, units, itemPos)){
+         if(!InputAndTools.doUnitsMatch(newUnit, units, itemPos)){
             System.out.println("The item is currently being stored in a different unit.");
             System.out.println("Please change it before attempting again.");
          }
@@ -643,9 +392,9 @@ public class SmartFridge{
          
       //actually adds the item to the arrays here
       if(itemPos == -1){
-         items[getNullPosition(quantity)] = newItem;
-         units[getNullPosition(quantity)] = newUnit;
-         quantity[getNullPosition(quantity)] = newQuantity;
+         items[InputAndTools.getNullPosition(quantity)] = newItem;
+         units[InputAndTools.getNullPosition(quantity)] = newUnit;
+         quantity[InputAndTools.getNullPosition(quantity)] = newQuantity;
          
          if(newUnit.equalsIgnoreCase("")){
             System.out.println("\nYou just stored " + newQuantity + " " + newItem);
@@ -657,7 +406,7 @@ public class SmartFridge{
          
       else{
          //convert units here
-         convertedQuantity = convertUnits(newQuantity, newUnit, itemPos, units);
+         convertedQuantity = InputAndTools.convertUnits(newQuantity, newUnit, itemPos, units);
          
          //new, converted quantity is set to the empty position
          quantity[itemPos] += convertedQuantity;
